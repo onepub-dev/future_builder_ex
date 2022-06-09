9902095852e0d10df18901bbbd809d3f09e06dc1
Brett Sutton <bsutton@noojee.com.au>
2022-06-09 11:41:43 +1000

    added copyright notices.

--- lib/src/empty.dart
+++ lib/src/empty.dart
@@ -1,3 +1,10 @@
+/* Copyright (C) S. Brett Sutton - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Brett Sutton <bsutton@onepub.dev>, Jan 2022
+ */
+
+
 import 'package:flutter/cupertino.dart';
 
 /// A convenience widget that returns a container
